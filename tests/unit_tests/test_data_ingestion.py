afd8a260ae6a7550100ea2e0ba12f1508b58fc4d
Rushikesh.Naik <rushikesh.naik@tigeranalytics.com>
2022-06-29 11:37:39 +0530

    commiting after figured out the issue of not able to push the changes to commit

--- tests/unit_tests/test_data_ingestion.py
+++ tests/unit_tests/test_data_ingestion.py
@@ -1,11 +1,21 @@
 import sys
 import pandas as pd
 
+import pytest
 sys.path.insert(1, "../../src/")
 from data_ingest import ingest_data
 
+@pytest.fixture
+def housing():
+    return pd.read_csv(r"C:\Users\rushikesh.naik\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs\home\rushikesh\assignment__1_2\mle-training\data\raw\housing\housing\housing.csv")
 
-def test_type_loading_housing_data():
+@pytest.fixture
+def processed_datapath():
+    return r"C:\Users\rushikesh.naik\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs\home\rushikesh\assignment__1_2\mle-training\data\processed\housing_prepared.csv"
+
+
+
+def test_shape_loading_housing_data():
 
     df = ingest_data.load_housing_data("C:/Users/rushikesh.naik/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/rushikesh/assignment__1_2/mle-training/data/raw/housing/housing")
     assert (20640, 10) == df.shape
@@ -13,4 +23,8 @@ def test_type_loading_housing_data():
 
 #test_type_loading_housing_data()
 
+def test_data_preprocessing(housing):
+    assert data_preprocessing(housing, processed_datapath )
 
+def test_sdf():
+    pass
