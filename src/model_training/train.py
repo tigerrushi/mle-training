f3e93df178c27a56f249e40fd5e026d9e68b2f7d
Rushikesh.Naik <rushikesh.naik@tigeranalytics.com>
2022-07-11 17:37:15 +0530

    adding state of code after the completion of assignment 3.2

--- src/model_training/train.py
+++ src/model_training/train.py
@@ -1,12 +1,26 @@
 import argparse
 import configparser
 import pickle
+from scipy.stats import randint
 import sys
 import os
-sys.path.insert(1, r"C:\Users\rushikesh.naik\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs\home\rushikesh\assignment__1_2\mle-training\logs")
+import mlflow
+import mlflow.sklearn
+
+
+sys.path.insert(
+    1,
+    "logs",
+)
 
 import logger_01 as l
-logger = l.configure_logger(log_file=os.path.join(r"C:\Users\rushikesh.naik\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs\home\rushikesh\assignment__1_2\mle-training\logs\logging_files","custom_config.log"))
+
+logger = l.configure_logger(
+    log_file=os.path.join(
+        "logs/logging_files",
+        "custom_config.log",
+    )
+)
 logger.info("Starting train.py")
 import numpy as np
 import pandas as pd
@@ -21,9 +35,7 @@ from sklearn.model_selection import (
 from sklearn.tree import DecisionTreeRegressor
 
 
-def model_training(
-    model_instance, housing_prepared, housing_labels, model_path
-):
+def model_training(model_instance, housing_prepared, housing_labels, model_path):
     logger.info("Training Started")
     model_instance.fit(housing_prepared, housing_labels)
     logger.info("Training COmpleted")
@@ -39,12 +51,165 @@ def model_training(
     )
 
 
+def training_with_gridsearchCV(
+    housing_prepared, housing_labels, model_path, strat_test_set, exp_name
+):
+    EXPERIMENT_NAME = exp_name
+    EXPERIMENT_ID = mlflow.create_experiment(EXPERIMENT_NAME)
+
+    with mlflow.start_run(experiment_id=EXPERIMENT_ID, run_name="run 1"):
+
+        with mlflow.start_run(
+            experiment_id=EXPERIMENT_ID, run_name="linear_regression", nested=True
+        ):
+
+            lin_reg = LinearRegression()
+            lin_reg.fit(housing_prepared, housing_labels)
+
+            housing_predictions = lin_reg.predict(housing_prepared)
+            lin_mse = mean_squared_error(housing_labels, housing_predictions)
+
+            lin_rmse = np.sqrt(lin_mse)
+            lin_rmse
+
+            lin_mae = mean_absolute_error(housing_labels, housing_predictions)
+            lin_mae
+            mlflow.log_metrics({"MSE": lin_mse, "RMSE": lin_rmse, "MAE": lin_mae})
+            mlflow.sklearn.log_model(sk_model=lin_reg, artifact_path="model")
+            pickle.dump(
+                lin_reg,
+                open(model_path + "/linear_regresion.pkl", "wb"),
+            )
+        with mlflow.start_run(
+            experiment_id=EXPERIMENT_ID, run_name="Regression_Tree", nested=True
+        ):
+            tree_reg = DecisionTreeRegressor(random_state=42)
+            tree_reg.fit(housing_prepared, housing_labels)
+
+            housing_predictions = tree_reg.predict(housing_prepared)
+            tree_mse = mean_squared_error(housing_labels, housing_predictions)
+            tree_rmse = np.sqrt(tree_mse)
+            tree_rmse
+            mlflow.log_metrics({"MSE": tree_mse, "RMSE": tree_rmse})
+            mlflow.sklearn.log_model(sk_model=tree_rmse, artifact_path="model")
+
+            pickle.dump(
+                tree_reg,
+                open(model_path + "/Decision_tree_reg.pkl", "wb"),
+            )
+        mlflow.sklearn.autolog()
+        with mlflow.start_run(
+            experiment_id=EXPERIMENT_ID, run_name="RandomForestTree", nested=True
+        ):
+            param_distribs = {
+                "n_estimators": randint(low=1, high=200),
+                "max_features": randint(low=1, high=8),
+            }
+
+            forest_reg = RandomForestRegressor(random_state=42)
+            rnd_search = RandomizedSearchCV(
+                forest_reg,
+                param_distributions=param_distribs,
+                n_iter=10,
+                cv=2,
+                scoring="neg_mean_squared_error",
+                random_state=42,
+            )
+            rnd_search.fit(housing_prepared, housing_labels)
+            cvres = rnd_search.cv_results_
+            for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+                print(np.sqrt(-mean_score), params)
+
+        with mlflow.start_run(
+            experiment_id=EXPERIMENT_ID, run_name="GridSearchCV", nested=True
+        ):
+            param_grid = [
+                # try 12 (3×4) combinations of hyperparameters
+                {"n_estimators": [3, 10, 30], "max_features": [2, 4, 6, 8]},
+                # then try 6 (2×3) combinations with bootstrap set as False
+                {
+                    "bootstrap": [False],
+                    "n_estimators": [3, 10],
+                    "max_features": [2, 3, 4],
+                },
+            ]
+
+            forest_reg = RandomForestRegressor(random_state=42)
+            # train across 5 folds, that's a total of (12+6)*5=90 rounds of training
+            grid_search = GridSearchCV(
+                forest_reg,
+                param_grid,
+                cv=2,
+                scoring="neg_mean_squared_error",
+                return_train_score=True,
+            )
+            grid_search.fit(housing_prepared, housing_labels)
+
+            grid_search.best_params_
+            cvres = grid_search.cv_results_
+            for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+                print(np.sqrt(-mean_score), params)
+
+            feature_importances = grid_search.best_estimator_.feature_importances_
+            sorted(zip(feature_importances, housing_prepared.columns), reverse=True)
+
+            final_model = grid_search.best_estimator_
+            print("Final Model : ", final_model)
+        mlflow.sklearn.autolog(disable=True)
+
+
+def run_model_training(exp_name):
+
+    logger.info("Configparser instantiating")
+    config = configparser.ConfigParser()
+
+    path = "config.ini"
+    config.read(path)
+    logger.info("Initializing Default parameters")
+    housing_prepared = config["ProcessedData"]["housing_prepared"]
+    housing_labels = config["ProcessedData"]["housing_labels"]
+    strat_test_set = config["ProcessedData"]["strat_test_set"]
+    model_path = config["ProcessedData"]["model_path"]
+
+    logger.info("Instantiating Argument Parser")
+
+    logger.info("Reading CSV files from default parameters")
+    housing_prepared = pd.read_csv(housing_prepared)
+    housing_labels = pd.read_csv(housing_labels)
+    strat_test_set = pd.read_csv(strat_test_set)
+
+    logger.info("Calling Model_Training Function")
+    # model_training(LinearRegression(), housing_prepared, housing_labels, model_path)
+
+    # model_training(
+    #     DecisionTreeRegressor(random_state=42),
+    #     housing_prepared,
+    #     housing_labels,
+    #     model_path,
+    # )
+
+    # model_training(
+    #     RandomForestRegressor(n_estimators=100, random_state=42),
+    #     housing_prepared,
+    #     housing_labels,
+    #     model_path,
+    # )
+
+    training_with_gridsearchCV(
+        housing_prepared, housing_labels, model_path, strat_test_set, exp_name
+    )
+
+    logger.info(
+        "Execution completed\n**********************************************************"
+    )
+
+
 if __name__ == "__main__":
 
     logger.info("Configparser instantiating")
     config = configparser.ConfigParser()
 
-    path = "C:/Users/rushikesh.naik/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/rushikesh/assignment__1_2/mle-training/config.ini"
+    path = "config.ini"
     config.read(path)
     logger.info("Initializing Default parameters")
     housing_prepared = config["ProcessedData"]["housing_prepared"]
@@ -55,25 +220,27 @@ if __name__ == "__main__":
     logger.info("Instantiating Argument Parser")
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--housing_prepared", help="Enter the output folder path to store the dataset", default=housing_prepared
+        "--housing_prepared",
+        help="Enter the output folder path to store the dataset",
+        default=housing_prepared,
     )
     parser.add_argument(
         "--housing_labels",
         help="Enter the  processed data folder path to store the \
-         dataset ready for modeling and inference",default=housing_labels
-
+         dataset ready for modeling and inference",
+        default=housing_labels,
     )
     parser.add_argument(
         "--strat_test_set",
         help="Enter the  processed data folder path to store the \
-         dataset ready for modeling and inference",default=strat_test_set
-
+         dataset ready for modeling and inference",
+        default=strat_test_set,
     )
     parser.add_argument(
         "--model_path",
         help="Enter the  processed data folder path to store the \
-         dataset ready for modeling and inference",default=model_path
-
+         dataset ready for modeling and inference",
+        default=model_path,
     )
     args = parser.parse_args()
 
@@ -88,17 +255,22 @@ if __name__ == "__main__":
     strat_test_set = pd.read_csv(strat_test_set)
 
     logger.info("Calling Model_Training Function")
+    model_training(LinearRegression(), housing_prepared, housing_labels, model_path)
+
     model_training(
-        LinearRegression(), housing_prepared, housing_labels, model_path
+        DecisionTreeRegressor(random_state=42),
+        housing_prepared,
+        housing_labels,
+        model_path,
     )
 
     model_training(
-        DecisionTreeRegressor(random_state=42), housing_prepared, housing_labels, model_path
+        RandomForestRegressor(n_estimators=100, random_state=42),
+        housing_prepared,
+        housing_labels,
+        model_path,
     )
 
-    model_training(
-        RandomForestRegressor(n_estimators=100, random_state=42), housing_prepared, housing_labels, model_path
+    logger.info(
+        "Execution completed\n**********************************************************"
     )
-
-    logger.info("Execution completed\n**********************************************************")
-
